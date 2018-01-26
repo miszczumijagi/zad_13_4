17cc908e96379f355d5661a7a4eca57138519012
Kamil Stangret <kamilstangret@wp.pl>
2018-01-26 20:23:29 +0100

    Add app and modules with files.

---
+++ modules/timeFormat.js
@@ -0,0 +1,20 @@
+var os = require('os');
+
+function timeFormating() {
+
+	var second;
+	var minute;
+	var hour;
+	var totalTime;
+	var time;
+	uptime = os.uptime();
+	hour = totalTime / 3600;
+	minute = (totalTime % 3600) / 60;
+	second = (totalTime % 3600) % 60;
+	time = Math.round(hour,0) + 'h ' + Math.round(minute,0) + 'min ' + second + 'sec';
+	console.log(time);
+};
+
+exports.print = timeFormating;
+
+
